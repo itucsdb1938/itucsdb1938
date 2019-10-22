4915730ec322be0de48f92d2bc60e960ab3c07d1
Egehan Orta <egehanorta@gmail.com>
2019-10-22 22:26:15 +0300

    Made connection with SQL Server

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
+import os
+import sys
+
+import psycopg2 as dbapi2
+
 from flask import Flask
+
+
+url = "dbname='snlvpekr' user='snlvpekr' host='balarama.db.elephantsql.com' password='EiWIdrfC_c6yWoP6Ln1goR5uq-Dl2hnp'" 
+
 
 
 app = Flask(__name__)
@@ -6,7 +15,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello ege!"
+    return """Hello ege!
+        <a href="xx">deneme</a>
+    """
+
+@app.route("/xx")
+def xx_page():
+
+    connection = dbapi2.connect(url)
+    cursor = connection.cursor()
+
+    query_string = "SELECT * FROM dummy"
+
+    cursor.execute(query_string)
+
+    data = cursor.fetchall()
+
+    connection.close()
+
+    print(data)
+
+    return str(data[0])
 
 
 if __name__ == "__main__":
