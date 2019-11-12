902ed9d5fb31ff22c6c5ce98e34bdf75b1be0356
Egehan Orta <egehanorta@gmail.com>
2019-11-12 20:44:13 +0300

    Created add, list, delete and edit pages for providers. Added homepage. Syntax fixed.

--- forms.py
+++ forms.py
@@ -8,50 +8,120 @@ class MarketPlace:
     def MarketPlace_add(self, name, address, authority, phonenumber, taxid, commission):
         dbconnection = dbapi.connect(url)
         cursor = dbconnection.cursor()
-        queryString = """INSERT INTO Marketplace (name, address, authority, phonenumber, taxid, commissionfee) VALUES (%s, %s, %s, %s, %s, %s)"""
+        queryString = """INSERT INTO Marketplace (name, address, authority, phonenumber, taxid, commissionfee) VALUES (%s, %s, %s, %s, %s, %s);"""
         cursor.execute(queryString, (name, address, authority, phonenumber, taxid, commission,))
         dbconnection.commit()
         cursor.close()
         dbconnection.close()
     
-    def MarketPlace_select(self, id, name):
+    def MarketPlace_select(self, market_id, name):
         dbconnection = dbapi.connect(url)
         cursor = dbconnection.cursor()
-        if (id == '*' or name == '*'):
-            queryString = """SELECT * FROM Marketplace ORDER BY MarketID ASC"""
+        if (market_id == '*' or name == '*'):
+            queryString = """SELECT * FROM Marketplace ORDER BY MarketID ASC;"""
             cursor.execute(queryString)
             selection = cursor.fetchall()
+            dbconnection.commit()
             cursor.close()
+            dbconnection.close()
             return selection
-        elif (id == '' and name != ''):
-            queryString = """SELECT * FROM Marketplace WHERE name = %s ORDER BY MarketID ASC"""
+        elif (market_id == '' and name != ''):
+            queryString = """SELECT * FROM Marketplace WHERE name = %s ORDER BY MarketID ASC;"""
             cursor.execute(queryString, (name,))
             selection = cursor.fetchall()
+            dbconnection.commit()
             cursor.close()
+            dbconnection.close()
             return selection
-        elif (id != '' and name == ''):
-            queryString = """SELECT * FROM Marketplace WHERE MarketID = %s ORDER BY MarketID ASC"""
-            cursor.execute(queryString, (id,))
+        elif (market_id != '' and name == ''):
+            queryString = """SELECT * FROM Marketplace WHERE MarketID = %s ORDER BY MarketID ASC;"""
+            cursor.execute(queryString, (market_id,))
             selection = cursor.fetchall()
+            dbconnection.commit()
             cursor.close()
+            dbconnection.close()
             return selection
         else:
+            cursor.close()
+            dbconnection.commit()
+            dbconnection.close()
             return
     
     def MarketPlace_delete(self, market_id):
         dbconnection = dbapi.connect(url)
         cursor = dbconnection.cursor()
-        queryString = """DELETE FROM Marketplace WHERE MarketID = %s"""
+        queryString = """DELETE FROM Marketplace WHERE MarketID = %s;"""
         cursor.execute(queryString, (market_id,))
         dbconnection.commit()
         cursor.close()
         dbconnection.close()
 
-    def MarketPlace_edit(self, name, address, authority, phonenumber, taxid, commission, market_id):
+    def MarketPlace_edit(self, market_id, name, address, authority, phonenumber, taxid, commission):
         dbconnection = dbapi.connect(url)
         cursor = dbconnection.cursor()
-        queryString = """UPDATE Marketplace SET name = %s, address = %s, authority = %s, phonenumber = %s, taxid = %s, commissionfee = %s  WHERE  MarketID = %s"""
+        queryString = """UPDATE Marketplace SET name = %s, address = %s, authority = %s, phonenumber = %s, taxid = %s, commissionfee = %s  WHERE  MarketID = %s;"""
         cursor.execute(queryString, (name, address, authority, phonenumber, taxid, commission, market_id,))
+        dbconnection.commit()
+        cursor.close()
+        dbconnection.close()
+
+class Provider:
+    def Provider_add(self, company, address, phone, taxid, authority):
+        dbconnection = dbapi.connect(url)
+        cursor = dbconnection.cursor()
+        queryString = """INSERT INTO Provider (Company, Address, Phone, TaxID, Authority) VALUES (%s, %s, %s, %s, %s);"""
+        cursor.execute(queryString, (company, address, phone, taxid, authority,))
+        dbconnection.commit()
+        cursor.close()
+        dbconnection.close()
+    
+    def Provider_select(self, provider_id, company):
+        dbconnection = dbapi.connect(url)
+        cursor = dbconnection.cursor()
+        if (provider_id == '*' or company == '*'):
+            queryString = """SELECT * FROM Provider;"""
+            cursor.execute(queryString)
+            selection = cursor.fetchall()
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
+            return selection
+        elif (provider_id == '' and company != ''):
+            queryString = """SELECT * FROM Provider WHERE company = %s;"""
+            cursor.execute(queryString, (company,))
+            selection = cursor.fetchall()
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
+            return selection
+        elif (provider_id != '' and company == ''):
+            queryString = """SELECT * FROM Provider WHERE ProviderID = %s;"""
+            cursor.execute(queryString, (provider_id,))
+            selection = cursor.fetchall()
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
+            return selection
+        else:
+            cursor.close()
+            dbconnection.commit()
+            dbconnection.close()
+            return
+
+    def Provider_delete(self, provider_id):
+        dbconnection = dbapi.connect(url)
+        cursor = dbconnection.cursor()
+        queryString = """DELETE FROM Provider WHERE ProviderID = %s;"""
+        cursor.execute(queryString, (provider_id,))
+        dbconnection.commit()
+        cursor.close()
+        dbconnection.close()
+
+    def Provider_edit(self, provider_id, company, address, phone, taxid, authority):
+        dbconnection = dbapi.connect(url)
+        cursor = dbconnection.cursor()
+        queryString = """UPDATE Provider SET Company = %s, Address = %s, Phone = %s, TaxID = %s, Authority = %s WHERE ProviderID = %s;"""
+        cursor.execute(queryString, (company, address, phone, taxid, authority, provider_id,))
         dbconnection.commit()
         cursor.close()
         dbconnection.close()
--- server.py
+++ server.py
@@ -13,9 +13,22 @@ url = "dbname='snlvpekr' user='snlvpekr' host='balarama.db.elephantsql.com' pass
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home_page():
-    return render_template('base.html',errorMessage="deneme")
+    if (request.method == 'GET'):
+        return render_template('homepage.html')
+
+    if (request.method == 'POST'):
+        if (request.form['submit_button'] == 'Marketplace Add'):
+            return redirect(url_for('marketplace_add'))
+        elif (request.form['submit_button'] == 'Marketplace List'):
+            return redirect(url_for('marketplace_list'))
+        elif (request.form['submit_button'] == 'Provider Add'):
+            return redirect(url_for('provider_add'))
+        elif (request.form['submit_button'] == 'Provider List'):
+            return redirect(url_for('provider_list'))
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
 
 @app.route("/marketplace_add", methods=['GET','POST'])
 def marketplace_add():
@@ -23,18 +36,18 @@ def marketplace_add():
         return render_template('marketplace_add.html')
 
     elif request.method == 'POST':
-        market_name = request.form.get('market_name')
-        market_address = request.form.get('market_address')
-        market_authority = request.form.get('market_authority')
-        market_phonenumber = request.form.get('market_phonenumber')
-        market_taxid = request.form.get('market_taxid')
-        market_commisionfee = request.form.get('market_commission')
-        
-        obj = forms.MarketPlace()
-
-        obj.MarketPlace_add(market_name,market_address,market_authority,market_phonenumber,market_taxid,market_commisionfee)
-
-        return render_template('marketplace_add.html',errorMessage="Done")
+        if (request.form['submit_button'] == 'Submit'):
+            market_name = request.form.get('market_name')
+            market_address = request.form.get('market_address')
+            market_authority = request.form.get('market_authority')
+            market_phonenumber = request.form.get('market_phonenumber')
+            market_taxid = request.form.get('market_taxid')
+            market_commisionfee = request.form.get('market_commission')
+            obj = forms.MarketPlace()
+            obj.MarketPlace_add(market_name,market_address,market_authority,market_phonenumber,market_taxid,market_commisionfee)
+            return redirect(url_for('marketplace_add'))
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
  
 @app.route("/marketplace_list", methods=['GET','POST'])
 def marketplace_list():
@@ -46,42 +59,105 @@ def marketplace_list():
             option = request.form['options']
             obj = forms.MarketPlace()
             obj.MarketPlace_delete(option)
-            return render_template('marketplace_list.html')
+            return redirect(url_for('marketplace_list'))
 
         elif (request.form['submit_button'] == 'Edit Selected'):
             option = request.form['options']
-            return redirect(url_for('marketplace_edit', marketid = option))
+            return redirect(url_for('marketplace_edit', market_id = option))
 
         elif (request.form['submit_button'] == 'Submit'):
             market_id = request.form.get('market_id')
             market_name = request.form.get('market_name')
             obj = forms.MarketPlace()
-
             data = obj.MarketPlace_select(market_id, market_name)
             return render_template('marketplace_list.html', data = data)
 
-@app.route("/marketplace_edit/<marketid>", methods=['GET','POST'])
-def marketplace_edit(marketid):
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
+
+@app.route("/marketplace_edit/<market_id>", methods=['GET','POST'])
+def marketplace_edit(market_id):
     if request.method == 'GET':
         obj = forms.MarketPlace()
-        data = obj.MarketPlace_select(marketid, '')
+        data = obj.MarketPlace_select(market_id, '')
         return render_template('marketplace_edit.html', data = data)
 
     elif request.method == 'POST':
-        market_name = request.form.get('market_name')
-        market_address = request.form.get('market_address')
-        market_authority = request.form.get('market_authority')
-        market_phonenumber = request.form.get('market_phonenumber')
-        market_taxid = request.form.get('market_taxid')
-        market_commisionfee = request.form.get('market_commission')
-        market_id = request.form['options']
-        obj = forms.MarketPlace()
+        if (request.form['submit_button'] == 'Submit'):
+            market_name = request.form.get('market_name')
+            market_address = request.form.get('market_address')
+            market_authority = request.form.get('market_authority')
+            market_phonenumber = request.form.get('market_phonenumber')
+            market_taxid = request.form.get('market_taxid')
+            market_commisionfee = request.form.get('market_commission')
+            obj = forms.MarketPlace()
+            obj.MarketPlace_edit(market_id,market_name,market_address,market_authority,market_phonenumber,market_taxid,market_commisionfee)
+            return redirect(url_for('marketplace_list'))
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
 
-        obj.MarketPlace_edit(market_name,market_address,market_authority,market_phonenumber,market_taxid,market_commisionfee,market_id)
-        return redirect(url_for('marketplace_list'))
+@app.route("/provider_add", methods=['GET', 'POST'])
+def provider_add ():
+    if request.method == 'GET':
+        return render_template('provider_add.html')
+    elif request.method == 'POST':
+        if (request.form['submit_button'] == 'Submit'):
+            provider_company = request.form.get('provider_company')
+            provider_address = request.form.get('provider_address')
+            provider_phonenumber = request.form.get('provider_phonenumber')
+            provider_taxid = request.form.get('provider_taxid')
+            provider_authority = request.form.get('provider_authority')
+            obj = forms.Provider()
+            obj.Provider_add(provider_company, provider_address, provider_phonenumber, provider_taxid, provider_authority)
+            return redirect(url_for('provider_add'))
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
 
+@app.route("/provider_list",methods=['GET', 'POST'])
+def provider_list ():
+    if request.method == 'GET':
+        return render_template('provider_list.html')
 
+    elif request.method == 'POST':
+        if (request.form['submit_button'] == 'Delete Selected'):
+            option = request.form['options']
+            obj = forms.Provider()
+            obj.Provider_delete(option)
+            return redirect(url_for('provider_list'))
 
+        elif (request.form['submit_button'] == 'Edit Selected'):
+            option = request.form['options']
+            return redirect(url_for('provider_edit', provider_id = option))
+
+        elif (request.form['submit_button'] == 'Submit'):
+            provider_id = request.form.get('provider_id')
+            provider_company = request.form.get('provider_company')
+            obj = forms.Provider()
+            data = obj.Provider_select(provider_id, provider_company)
+            return render_template('provider_list.html', data = data)
+
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
+
+@app.route("/provider_edit/<provider_id>",methods=['GET', 'POST'])
+def provider_edit (provider_id):
+    if request.method == 'GET':
+        obj = forms.Provider()
+        data = obj.Provider_select(provider_id, '')
+        return render_template('provider_edit.html', data = data)
+
+    if request.method == 'POST':
+        if (request.form['submit_button'] == 'Submit'):
+            provider_company = request.form.get('provider_company')
+            provider_address = request.form.get('provider_address')
+            provider_phonenumber = request.form.get('provider_phonenumber')
+            provider_taxid = request.form.get('provider_taxid')
+            provider_authority = request.form.get('provider_authority')
+            obj = forms.Provider()
+            obj.Provider_edit(provider_id, provider_company, provider_address, provider_phonenumber, provider_taxid, provider_authority)
+            return redirect(url_for('provider_list'))
+        elif (request.form['submit_button'] == 'Homepage'):
+            return redirect(url_for('home_page'))
 
 if __name__ == "__main__":
     app.run()
